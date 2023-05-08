975bcd9e4e819f90580c0c63f3c502dfa3a1c95c
Nathaniel Warner <natedawg3124@gmail.com>
2023-05-08 13:59:21 -0700

    Initial commit.

---
+++ src/main/java/com/nwarner/projasstracker/ProjassTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.nwarner.projasstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjassTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjassTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nwarner/projasstracker/ProjassTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwarner.projasstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjassTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
